8665789f95487c0174859a995e321ace8f603ea8
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2020-08-13 19:20:25 +0530

    Create Print_hello_world_thread_private.c

---
+++ Print_hello_world_thread_private.c
@@ -0,0 +1,19 @@
+//Print Hello world with different number of threads using private variable
+
+#include <omp.h>
+#include<stdio.h>
+int main (){
+int nthreads, tid;
+/* Fork a team of threads with each thread
+having a private tid variable */
+#pragma omp parallel private(tid){
+/* Obtain and print thread id */
+tid = omp_get_thread_num();
+printf("Hello World from thread = %d\n",
+tid);
+if (tid == 0) { nthreads =
+omp_get_num_threads(); printf("Number of
+threads = %d\n", nthreads); } }
+/* All threads join master thread and
+terminate */
+}
