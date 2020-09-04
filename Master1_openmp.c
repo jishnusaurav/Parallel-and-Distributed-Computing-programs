b2ed49cbc51c3928fa8afc86d28c67b51870d3b5
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2020-09-04 10:41:56 +0530

    Create Master1_openmp.c

---
+++ Master1_openmp.c
@@ -0,0 +1,12 @@
+#include <omp.h>
+#define _GNU_SOURCE
+#include<stdio.h>
+#include <sched.h>
+int main()
+{
+#pragma omp parallel{
+printf("Hello world!!\n");
+#pragma omp master
+printf("%d\n", omp_get_num_threads());
+}
+}
